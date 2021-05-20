33ec30b13cb371b4de196c746d3474094f65654e
sh0ckR6 <sh0ck.04@outlook.com>
2021-05-20 18:57:30 -0400

    Implement count_numbers function

--- textprocessors.cpp
+++ textprocessors.cpp
@@ -11,7 +11,14 @@ int count_words(char *text) {
 }
 
 int count_numbers(char *text) {
-    return 0;
+    int count = 0;
+    for (int i = 0; text[i] != '\0'; i++) {
+        char currentChar = text[i];
+        if (isdigit(currentChar)) {
+            count++;
+        }
+    }
+    return count;
 }
 
 void lowercase_letters(char *text) {
