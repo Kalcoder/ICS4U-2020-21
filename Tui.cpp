b855bbc0b6f8f89945dd9831e9379f70b7247746
sh0ckR6 <sh0ck.04@outlook.com>
2021-06-17 12:29:24 -0400

    Add framework for the text UI

---
+++ Tui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kaleb on 2021-06-16.
+//
+
+#include "Tui.h"
---
+++ Tui.h
@@ -0,0 +1,14 @@
+//
+// Created by kaleb on 2021-06-16.
+//
+
+#ifndef FINAL_PROJECT_TUI_H
+#define FINAL_PROJECT_TUI_H
+
+
+class Tui {
+
+};
+
+
+#endif //FINAL_PROJECT_TUI_H
