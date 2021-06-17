c5979bf946f07ecf1c0176d8fd41edd317519a6d
sh0ckR6 <sh0ck.04@outlook.com>
2021-06-17 15:26:45 -0400

    Remove unnecessary char * versions of text processors
    This was already done in ktextprocessors.h/.cpp when splitting files

--- jtextprocessors.cpp
+++ jtextprocessors.cpp
@@ -6,26 +6,14 @@
 
 // Johnny Text Processors
 
-std::string leet_case_char(char *text) {
-    return "test";
-}
-
 std::string leet_case(std::string text) {
-    return leet_case_char(&text[0]);
-}
-
-std::string capitalize_letters_char(char *text) {
     return "test";
 }
 
 std::string capitalize_letters(std::string text) {
-    return capitalize_letters_char(&text[0]);
-}
-
-std::string search_replace_char(char *text, char *search, char *replace) {
     return "test";
 }
 
 std::string search_replace(std::string text, std::string search, std::string replace) {
-    return search_replace_char(&text[0], &search[0], &replace[0]);
+    return "test";
 }
--- jtextprocessors.h
+++ jtextprocessors.h
@@ -7,11 +7,8 @@
 #include <string>
 
 // Johnny Text Processors
-std::string leet_case_char(char *text);
 std::string leet_case(std::string text);
-std::string capitalize_letters_char(char *text);
 std::string capitalize_letters(std::string text);
-std::string search_replace_char(char *text, char *search, char *replace);
 std::string search_replace(std::string text, std::string search, std::string replace);
 
 #endif //FINAL_PROJECT_TEXTPROCESSORS_H
